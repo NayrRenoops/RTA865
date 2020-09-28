c9b1ca01ee779c675bb4393ce3afc1529f4fd383
NayrRenoops <71355017+NayrRenoops@users.noreply.github.com>
2020-09-27 22:07:06 -0400

    Completed Engine Basics A2
    Completed the rube goldberg assignment

---
+++ Assets/Scripts/jumpPad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class jumpPad : MonoBehaviour {
+ 
+     public int speed;
+ 
+     void OnCollisionEnter(Collision other)
+     {
+         if (other.gameObject.CompareTag("JumpPad"))
+         {
+             other.gameObject.GetComponent<Rigidbody>
+                     ().AddForce(Vector3.up*speed);
+         }
+     }
+ }
